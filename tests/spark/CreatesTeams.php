15be3aba4da3a2ea9d854b1fa970469d4261fa5e
Jeremy Anderson <jeremy@edos.io>
2016-11-28 10:12:21 -0700

    Some spark tests, committing the working ones, many more in the works

---
+++ tests/spark/CreatesTeams.php
@@ -0,0 +1,26 @@
+<?php
+
+use Groid\User;
+use Groid\Team;
+
+trait CreatesTeams
+{
+        /**
+         * Create a new team instance.
+         *
+         * @return \Laravel\Spark\Team
+         */
+        public function createTeam($user = null, $role = 'owner')
+        {
+            $user = $user ?: factory(User::class)->create();
+
+            $team = (new Team)->forceFill([
+                'name' => 'New Team',
+                'owner_id' => $user->id,
+            ]);
+
+            $user->teams()->save($team, ['role' => $role]);
+
+            return $team->fresh();
+        }
+}
